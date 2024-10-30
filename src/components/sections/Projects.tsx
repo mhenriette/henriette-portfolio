e6786d106fb073f83092ca777d22bbb38c5b5078
mhenriette <mhenriette00@gmail.com>
2024-10-30 15:55:49 +0200

    fix: projects responsiveness

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -10,12 +10,15 @@ export default function Projects() {
     setShow(value)
   }
   return (
-    <div className="w-full max-w-6xl mx-auto px-3 py-8 md:py-12 flex flex-col" id="projects">
+    <div
+      className="w-full max-w-6xl mx-auto px-3 py-8 md:py-12 flex flex-col"
+      id="projects"
+    >
       <h2 className="font-bold text-2xl md:text-3xl text-start">
         A small Selection of the Recent
         <span className="text-secondary"> Projects</span>
       </h2>
-      <div className="grid justify-center md:grid-cols-2 lg:grid-cols-3 w-full gap-10 max-w-6xl mt-10">
+      <div className="grid grid-cols-1 justify-center md:grid-cols-2 lg:grid-cols-3 w-full gap-10 mt-10">
         {projects.slice(0, 3).map((project) => (
           <Project
             key={project.id}
@@ -28,7 +31,11 @@ export default function Projects() {
           />
         ))}
       </div>
-      <div className={`justify-center md:grid-cols-2 lg:grid-cols-3 w-full gap-10 max-w-6xl mt-10 ${ show ? "grid": "hidden"}`}>
+      <div
+        className={`grid grid-cols-1 justify-center md:grid-cols-2 lg:grid-cols-3 w-full gap-10  mt-10 ${
+          !show && "hidden"
+        }`}
+      >
         {projects.slice(3).map((project) => (
           <Project
             key={project.id}
@@ -41,7 +48,11 @@ export default function Projects() {
           />
         ))}
       </div>
-      <AnimatedButton onClick={()=> handleShow(!show)}>{show? "Show less": "Show more"}</AnimatedButton>
+      <div className="mt-8 flex justify-center">
+        <AnimatedButton onClick={() => handleShow(!show)}>
+          {show ? "Show less" : "Show more"}
+        </AnimatedButton>
+      </div>
     </div>
   );
 }
