1bf896a2b277e2487c4504a275ce19c119459f16
mhenriette <mhenriette00@gmail.com>
2025-01-22 13:54:24 +0200

    feat: add vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Space_Grotesk } from "@next/font/google"
+import { Analytics } from "@vercel/analytics/react";
 
 const spaceGrotesk = Space_Grotesk({
   subsets: ["latin"],
@@ -15,6 +16,7 @@ export default function App({ Component, pageProps }: AppProps) {
         }
       `}</style>
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
